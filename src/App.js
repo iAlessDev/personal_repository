d4ca64aa725827cfb8cd1b999f80895c2331e610
Paul Flores <paulfloresdev@icloud.com>
2024-11-27 19:28:49 -0600

    Add a hello world for test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello world from react and paulfloresdev!
         </a>
       </header>
     </div>
