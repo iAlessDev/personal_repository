0f0ae26b70dd0b75ed98dc5faf34e5ee04817e0b
Paul Flores <paulfloresdev@icloud.com>
2024-12-10 13:14:01 -0600

    Add a new archive for the text silder components

---
+++ src/components/Technologies/PortfolioTechnologies.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+
+function PortfolioTechnologies() {
+    return (
+        <div className="flex items-center justify-center space-x-4 font-funnel py-5 bg-[#E5E5E5] text-[#1E90FF]">
+            <div className="flex space-x-5 ">
+                <h1 className="font-bold ml-4">
+                    Databases:
+                </h1>
+                    <p>MySql</p>
+                    <p>MariaDB</p>
+                    <p>PostgreSQL</p>
+                    <p>OracleSQL</p>
+                    <p>MongoDB</p>
+            </div>
+            <div className="flex space-x-5">
+                <h1 className="font-bold">
+                    Web Technologies:
+                </h1>
+                <p>React</p>
+                <p>HTML</p>
+                <p>CSS</p>
+                <p>JavaScript</p>
+                <p>Astro</p>
+            </div>
+            <div className="flex space-x-5">
+                <h1 className="font-bold">
+                    Backend Technologies:
+                </h1>
+                <p>Java</p>
+                <p>PHP</p>
+                <p>C</p>
+                <p>Python</p>
+                <p>Bash</p>
+            </div>
+            <div className="flex space-x-5">
+                <h1 className="font-bold">
+                    Java Technologies:
+                </h1>
+                <p>Spring Boot</p>
+                <p>Spring Security</p>
+                <p>Spring Data</p>
+                <p>JUnit</p>
+                <p>Mockito</p>
+                <p>Maven</p>
+                <p>Gradle</p>
+                <p>Astro</p>
+            </div>
+            <div className="flex space-x-5 ">
+                <h1 className="font-bold">
+                    Other Technologies:
+                </h1>
+                <p>Github</p>
+                <p>Git</p>
+                <p>Docker</p>
+                <p>Kubernates</p>
+            </div>
+        </div>
+    )
+}
+
+export default PortfolioTechnologies;
