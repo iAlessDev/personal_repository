d31ddeac77ca75b9958919a4c56f02cb8a60da36
Paul Flores <paulfloresdev@icloud.com>
2024-12-09 19:32:15 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from './components/portfolio.jsx';
 import Home from './components/home.jsx';
 import Navbar from './components/navbar.jsx';
+import Blog from './components/blog.jsx';
+import Contact from './components/contact.jsx';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
-    </Router>
+  <Router>  
+      <div className="App">
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+    </div>
+  </Router>
   );
 }
 
