1f13c57649c0693b7e83a35d76378adeed5b1ba3
Paul Flores <paulfloresdev@icloud.com>
2024-12-10 13:12:11 -0600

    Add text slider for technologies

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { TypeAnimation } from 'react-type-animation';
+import Marquee from "react-fast-marquee";
+import Technologies from "./Technologies/PortfolioTechnologies";
+
+
 
 
 function Home() {
@@ -16,7 +20,6 @@ function Home() {
                         <h1 className="sm:text-6xl text-4xl bg-gradient-to-r from-[#0D1321] via-[#1E90FF] to-[#32CD32] inline-block text-transparent bg-clip-text font-bold">
                             I'm Paul
                         </h1>
-
                     </div>
                     
                     <h2 className="mx-12 bg-gradient-to-r from-[#FF5733] via-[#0077FF] to-[#6A0DAD] inline-block text-transparent bg-clip-text text-1xl sm:text-2xl font-bold">
@@ -60,6 +63,30 @@ function Home() {
                     </div>
 
                 </div>
+            </div>
+            <div className="bg-[#E5E5E5] w-full py-5 text-[#007AFF] border-gray-500">
+                <h2 className="text-center text-2xl font-funnel">
+                    IOS | WEB 
+                </h2>
+            </div>
+            <Marquee className="text-2xl"
+                pauseOnHover={true}
+                direction="left"
+            >
+                <Technologies />
+                
+            </Marquee>
+            <div className="flex justify-center space-x-36 w-full my-52">
+                <div className="flex flex-col">
+                    <h1 className="text-5xl">
+                        What about me?
+                    </h1>
+                    <p className="text-2xl mt-10">
+                        I'm Paul Flores, a freelance CDMX web developer & designer bringing you the
+                    </p>
+                </div>
+                
+                <img src="paulfloresdev.JPEG" alt="Paul Flores" className="w-[25%] rounded-full"/>
             </div>
         </div>
     );
