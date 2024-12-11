7d7e09075651dfee66953abd2628e0a1077333e0
Paul Flores <paulfloresdev@icloud.com>
2024-12-10 20:24:48 -0600

    Add a slider effect with 2 cards about me and my services

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { TypeAnimation } from 'react-type-animation';
 import Marquee from "react-fast-marquee";
 import Technologies from "./Technologies/PortfolioTechnologies";
-
-
-
+import { ReactComponent as Apple } from '../apple.svg';
+import { ReactComponent as Computer } from '../computer.svg'
 
 function Home() {
+
+const [activeTab, setActiveTab] = useState("IOS");
+const [hover, setHover] = useState(false);
+
     return (
         <div>
             <div>
@@ -64,11 +67,15 @@ function Home() {
 
                 </div>
             </div>
-            <div className="bg-[#E5E5E5] w-full py-5 text-[#007AFF] border-gray-500">
+            <div className="bg-[#E5E5E5] w-full py-5 text-[#1065bb] border-gray-500">
                 <h2 className="text-center text-2xl font-funnel">
                     IOS | WEB 
                 </h2>
             </div>
+            <div className="flex justify-center items-center bg-[#E5E5E5]">
+                <div className="w-[30%] h-0.5 mx-12 bg-gradient-to-r from-[#5e8ba0] via-[#a8ff05] to-[#32CD32] inline-block text-transparent text-1xl sm:text-2xl font-bold"></div>
+            </div>
+
             <Marquee className="text-2xl"
                 pauseOnHover={true}
                 direction="left"
@@ -76,20 +83,77 @@ function Home() {
                 <Technologies />
                 
             </Marquee>
-            <div className="flex justify-center space-x-36 w-full my-52">
-                <div className="flex flex-col">
-                    <h1 className="text-5xl">
-                        What about me?
-                    </h1>
-                    <p className="text-2xl mt-10">
-                        I'm Paul Flores, a freelance CDMX web developer & designer bringing you the
-                    </p>
+            <div className="flex space-x-8 w-full py-16 bg-[#E5E5E5] items-start">
+                <div className="flex justify-start space-x-3 w-[45%] border-[#be6700] border-2 py-5 rounded-2xl ml-12 bg-[#f6f5de] shadow-2xl h-auto">
+                    <img src="paulfloresdev.JPEG" alt="Paul Flores" className="h-40 mt-3 rounded-full border-[#FF6F61] border-2 ml-5"/>
+                    <div className="flex flex-col mr-5">
+                        <h1 className="text-5xl font-oswald">
+                            Hey there!
+                        </h1>
+                        <p className="text-2xl mt-5 w-full text-[#D1D1D1} font-funnel">
+                            I'm Paul Flores, a content creator, speaker and developer specialized in Web and IOS applications
+                        </p>
+                    </div>
                 </div>
-                
-                <img src="paulfloresdev.JPEG" alt="Paul Flores" className="w-[25%] rounded-full"/>
+                    <div className="flex flex-col h-auto w-[45%] border-transparent rounded-2xl bg-white shadow-lg">
+                        <div className="flex justify-center space-x-4 w-full p-4">
+                            <button 
+                                className="flex flex-col items-center w-1/2 py-3 text-lg font-light text-gray-800 bg-white  rounded-md  hover:scale-125 focus:ring-2 transition-all "
+                                onClick={() => setActiveTab("IOS")}                                
+                            >
+                                <Apple />
+                                IOS
+                            </button>
+                            <button 
+                                className="flex flex-col items-center w-1/2 py-3 text-lg font-light text-gray-800 bg-white  rounded-md hover:scale-125 focus:ring-2 transition-all"
+                                onClick={() => setActiveTab("WEB")}    
+                            >
+                                <Computer />
+                                WEB
+                            </button>
+                        </ div>
+                        <div className="w-full p-6 border rounded-md bg-gray-50 shadow-lg">
+                            {activeTab === "IOS" && (
+                                <div>
+                                    <h2 className="text-2xl font-semibold text-gray-800">IOS Development</h2>
+                                    <p className="text-gray-600 mt-2">
+                                        I develop native applications 
+                                        specifically for iOS devices, including iPhones and iPads. 
+                                        With expertise in <strong>Swift</strong>, I create high-performance, user-friendly apps 
+                                        that leverage the full potential of the iOS ecosystem. 
+                                    </p> 
+                                    <p className="text-gray-600 mt-3">
+                                        Whether building from the ground up or optimizing existing solutions, 
+                                        my focus is on delivering seamless, intuitive, and responsive applications 
+                                        that meet the highest standards for iOS development.
+                                    </p>
+                                </div>
+                            )}
+                            {activeTab === "WEB" && (
+                                <div>
+                                    <h2 className="text-2xl font-semibold text-gray-800">Web Development</h2>
+                                    <p className="text-gray-600 mt-2">
+                                        I offer web and backend development services, 
+                                        specializing in modern websites with 
+                                        <strong> HTML, CSS, JavaScript, React, and Astro, </strong>
+                                        and backend solutions using 
+                                        <strong> Java, PHP, Python, C, and Bash.</strong>
+                                    </p>
+                                    <p className="text-gray-600 mt-2">
+                                        I manage databases like <strong>MySQL, MariaDB, PostgreSQL, OracleSQL, and MongoDB,</strong>
+                                        and provide cloud hosting with <strong>AWS </strong> services such as <strong>S3, CloudFront, and EC2. </strong>
+                                    </p>
+                                    <p className="text-gray-600 mt-2">
+                                        I also ensure smooth deployment with <strong>Docker</strong> and version control using Git and GitHub, 
+                                        delivering efficient, high-performance applications tailored to your needs.
+                                    </p>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 
 export default Home;
