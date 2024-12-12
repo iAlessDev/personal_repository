5008105060feccaeb83d052faaeed15843c84627
Paul Flores <paulfloresdev@icloud.com>
2024-12-11 18:29:51 -0600

    Companies that have relied on me added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,10 +4,47 @@ import Marquee from "react-fast-marquee";
 import Technologies from "./Technologies/PortfolioTechnologies";
 import { ReactComponent as Apple } from '../apple.svg';
 import { ReactComponent as Computer } from '../computer.svg'
+import Slider from "react-slick";
 
 function Home() {
 
-const [activeTab, setActiveTab] = useState("IOS");
+    const [activeTab, setActiveTab] = useState("IOS");
+
+    const settings = {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        pauseOnHover: true,
+        arrows: false,
+        cssEase: "linear",
+        responsive: [
+
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                autoplay: true,
+                initialSlide: 0,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                autoplay: true,
+                dots: true
+              }
+            }
+        ]
+    };
 
     return (
         <div>
@@ -63,14 +100,15 @@ const [activeTab, setActiveTab] = useState("IOS");
                             <ion-icon size="large" name="logo-tiktok"></ion-icon>
                         </a>                    
                     </div>
-
                 </div>
             </div>
+
             <div className="bg-[#E5E5E5] w-full py-5 text-[#1065bb] border-gray-500">
                 <h2 className="text-center text-2xl font-funnel">
                     IOS | WEB 
                 </h2>
             </div>
+
             <div className="flex justify-center items-center bg-[#E5E5E5]">
                 <div className="w-[30%] h-0.5 mx-12 bg-gradient-to-r from-[#5e8ba0] via-[#a8ff05] to-[#32CD32] inline-block text-transparent text-1xl sm:text-2xl font-bold"></div>
             </div>
@@ -94,65 +132,90 @@ const [activeTab, setActiveTab] = useState("IOS");
                         </p>
                     </div>
                 </div>
-                    <div className="flex flex-col mb-16 mx-7 md:mx-0 h-auto w-[85%] md:w-[45%] border-transparent rounded-2xl bg-white shadow-lg">
-                        <div className="flex justify-center space-x-4 w-full p-4">
-                            <button 
-                                className="flex flex-col items-center w-1/2 py-3 text-lg font-light text-gray-800 bg-white  rounded-md  hover:scale-125 focus:ring-2 transition-all "
-                                onClick={() => setActiveTab("IOS")}                                
-                            >
-                                <Apple />
-                                IOS
-                            </button>
-                            <button 
-                                className="flex flex-col items-center w-1/2 py-3 text-lg font-light text-gray-800 bg-white  rounded-md hover:scale-125 focus:ring-2 transition-all"
-                                onClick={() => setActiveTab("WEB")}    
-                            >
-                                <Computer />
-                                WEB
-                            </button>
-                        </ div>
-                        <div className="w-full p-6 border rounded-md bg-gray-50 shadow-lg">
-                            {activeTab === "IOS" && (
-                                <div>
-                                    <h2 className="text-2xl font-semibold text-gray-800">IOS Development</h2>
-                                    <p className="text-gray-600 mt-2">
-                                        I develop native applications 
-                                        specifically for iOS devices, including iPhones and iPads. 
-                                        With expertise in <strong>Swift</strong>, I create high-performance, user-friendly apps 
-                                        that leverage the full potential of the iOS ecosystem. 
-                                    </p> 
-                                    <p className="text-gray-600 mt-3">
-                                        Whether building from the ground up or optimizing existing solutions, 
-                                        my focus is on delivering seamless, intuitive, and responsive applications 
-                                        that meet the highest standards for iOS development.
-                                    </p>
-                                </div>
-                            )}
-                            {activeTab === "WEB" && (
-                                <div>
-                                    <h2 className="text-2xl font-semibold text-gray-800">Web Development</h2>
-                                    <p className="text-gray-600 mt-2">
-                                        I offer web and backend development services, 
-                                        specializing in modern websites with 
-                                        <strong> HTML, CSS, JavaScript, React, and Astro, </strong>
-                                        and backend solutions using 
-                                        <strong> Java, PHP, Python, C, and Bash.</strong>
-                                    </p>
-                                    <p className="text-gray-600 mt-2">
-                                        I manage databases like <strong>MySQL, MariaDB, PostgreSQL, OracleSQL, and MongoDB, </strong>
-                                        and provide cloud hosting with <strong>AWS </strong> services such as <strong>S3, CloudFront, and EC2. </strong>
-                                    </p>
-                                    <p className="text-gray-600 mt-2">
-                                        I also ensure smooth deployment with <strong>Docker</strong> and version control using Git and GitHub, 
-                                        delivering efficient, high-performance applications tailored to your needs.
-                                    </p>
-                                </div>
-                            )}
-                        </div>
+                <div className="flex flex-col mb-16 mx-7 md:mx-0 h-auto w-[85%] md:w-[45%] border-transparent rounded-2xl bg-white shadow-lg">
+                    <div className="flex justify-center space-x-4 w-full p-4">
+                        <button 
+                            className="flex flex-col items-center w-1/2 py-3 text-lg font-light text-gray-800 bg-white  rounded-md  hover:scale-125 focus:ring-2 transition-all "
+                            onClick={() => setActiveTab("IOS")}                                
+                        >
+                            <Apple />
+                            IOS
+                        </button>
+                        <button 
+                            className="flex flex-col items-center w-1/2 py-3 text-lg font-light text-gray-800 bg-white  rounded-md hover:scale-125 focus:ring-2 transition-all"
+                            onClick={() => setActiveTab("WEB")}    
+                        >
+                            <Computer />
+                            WEB
+                        </button>
+                    </ div>
+                    <div className="w-full p-6 border rounded-md bg-gray-50 shadow-lg">
+                        {activeTab === "IOS" && (
+                            <div>
+                                <h2 className="text-2xl font-semibold text-gray-800">IOS Development</h2>
+                                <p className="text-gray-600 mt-2">
+                                    I develop native applications 
+                                    specifically for iOS devices, including iPhones and iPads. 
+                                    With expertise in <strong>Swift</strong>, I create high-performance, user-friendly apps 
+                                    that leverage the full potential of the iOS ecosystem. 
+                                </p> 
+                                <p className="text-gray-600 mt-3">
+                                    Whether building from the ground up or optimizing existing solutions, 
+                                    my focus is on delivering seamless, intuitive, and responsive applications 
+                                    that meet the highest standards for iOS development.
+                                </p>
+                            </div>
+                        )}
+
+                        {activeTab === "WEB" && (
+                            <div>
+                                <h2 className="text-2xl font-semibold text-gray-800">Web Development</h2>
+                                <p className="text-gray-600 mt-2">
+                                    I offer web and backend development services, 
+                                    specializing in modern websites with 
+                                    <strong> HTML, CSS, JavaScript, React, and Astro, </strong>
+                                    and backend solutions using 
+                                    <strong> Java, PHP, Python, C, and Bash.</strong>
+                                </p>
+                                <p className="text-gray-600 mt-2">
+                                    I manage databases like <strong>MySQL, MariaDB, PostgreSQL, OracleSQL, and MongoDB, </strong>
+                                    and provide cloud hosting with <strong>AWS </strong> services such as <strong>S3, CloudFront, and EC2. </strong>
+                                </p>
+                                <p className="text-gray-600 mt-2">
+                                    I also ensure smooth deployment with <strong>Docker</strong> and version control using Git and GitHub, 
+                                    delivering efficient, high-performance applications tailored to your needs.
+                                </p>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
-        );
-    }
+            <div className="bg-[#E5E5E5] pb-24 md:px-5">
+                    <h2 className="text-2xl md:text-4xl font-oswald text-center pb-8 md:pb-5 text-gray-800">
+                        Companies That Have Relied on Me
+                    </h2>
+                <div className=" bg-gray-900 rounded-xl shadow-2xl mx-12">
+                    <Slider {...settings}>
+                        <div className="md:ml-10 lg:ml-3 xl:ml-5">
+                            <a href="https://www.santander.com.mx">
+                                <img src="santander.png" alt="Santander logo"/>
+                            </a>
+                        </div>
+                        <div  className="scale-75 md:ml-10 lg:ml-3 xl:ml-5">
+                            <a href="https://softelligent-business.com.mx/">
+                                <img src="softelligent-business.png" alt="Softelligent Business Logo"/>
+                            </a>
+                        </div>
+                        <div className="pt-3 scale-125 md:ml-10 lg:ml-3 xl:ml-5">
+                            <a href="https://www.nocountry.tech/">
+                                <img src="no-country-logo.png" alt="No country logo"/>
+                            </a>
+                        </div>
+                    </Slider>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default Home;
