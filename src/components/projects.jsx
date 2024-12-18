1d621de0d900689b07e91a5144c776c5f2ab295d
Paul Flores <paulfloresdev@icloud.com>
2024-12-18 17:43:30 -0600

    Add project initial page

---
+++ src/components/projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>My Projects</h1>
+            <p>This component is in construction</p>
+        </div>
+    );
+};
+
+export default Projects;
