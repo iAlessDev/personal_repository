25a4fe39c041f5134489b2ea386fbff6e1cf7b7c
Paul Flores <paulfloresdev@icloud.com>
2025-02-28 17:08:36 -0600

    Add responsive design

--- src/components/projectDetails.jsx
+++ src/components/projectDetails.jsx
@@ -24,22 +24,21 @@ const Projects = () => {
     } else {
         console.log("Project found");
     }
-
     return (
-        <div className="flex flex-row w-full my-28 space-x-72 justify-center">
-            <div className="w-2/6 max-h-fit p-10 bg-[#F5F5F7] rounded-xl">
+        <div className="flex flex-col md:flex-row w-full md:my-28 md:space-x-72 justify-center">
+
+            <div className="w-5/6 md:w-2/6 max-h-fit p-10 m-10 bg-[#F5F5F7] rounded-xl">
                 <h1 className="text-5xl font-Inter font-bold mb-4 text-[#1D1D1F]">{project.text}</h1>
                 <p className="text-lg text-[#515154] mb-10">{project.usage}</p>
                 <h2 className="text-3xl font-Inter">How I did it?</h2>
                 <p className="text-lg text-[#515154] font-sans mb-10">
-
-                {project.description.split('\n').map((line, index) => (
-                    <React.Fragment key={index}>
-                    {line}
-                    <br />
-                    </React.Fragment>
-                ))}
-</p>
+                    {project.description.split('\n').map((line, index) => (
+                        <React.Fragment key={index}>
+                        {line}
+                        <br />
+                        </React.Fragment>
+                    ))}
+                </p>
                 <h2 className="text-3xl"> What I learned?</h2>
                 <p className="text-lg text-[#515154] font-sans mb-5">{project.skills}</p>
 
@@ -57,15 +56,15 @@ const Projects = () => {
                 </div>
             </div>
     
-            <div className="w-1/5">
+            <div className="w-5/6 md:w-1/5 m-10 md:m-0">
                 <img
                     src={`/${project.src}`}
                     alt={project.alt}
                     className="w-full h-auto object-cover rounded-lg mb-4"
                 />
-                <p className="text-lg text-gray-600 font-sans">
+                <p className="mx-10 text-lg text-gray-600 font-sans">
                     {project.technologies.map((tech, index) => (
-                        <span key={index} className="inline-block bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-sm mr-2">
+                        <span key={index} className="inline-block bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-sm mr-2 m-1">
                             {tech}
                         </span>
                     ))}
